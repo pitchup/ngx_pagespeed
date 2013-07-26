8ba9826d8037d006a34a9228565f14c8b2c859aa
Matthew Jacobi <matt@oppian.com>
2013-07-26 14:28:37 +0100

    build script

--- build_deb.sh
+++ build_deb.sh
@@ -6,11 +6,13 @@ sudo apt-get update
 # dpkg needed for building debs
 sudo apt-get install dpkg-dev
 
-# install the nginx source
-sudo apt-get source nginx
-
 # install any packages needed for nginx
-sudo apt-get build-dep nginx
+sudo apt-get build-dep nginx-1.4.2
 
 # deps for pagespeed
-sudo apt-get install build-essential zlib1g-dev libpcre3 libpcre3-dev
+sudo apt-get install build-essential zlib1g-dev libpcre3 libpcre3-dev libssl-dev
+
+# install the nginx sourc
+mkdir build
+cd build
+apt-get source nginx-1.4.2
