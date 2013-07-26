ddab6f0ba637e2df90522e0728a7c479f80f4aa3
Matthew Jacobi <matt@oppian.com>
2013-07-26 14:38:27 +0100

    adding psol to build script

--- build_deb.sh
+++ build_deb.sh
@@ -1,5 +1,7 @@
 #/bin/bash -x
 
+NGINX_VERSION=1.4.2
+
 # update list of packages
 sudo apt-get update
 
@@ -7,12 +9,25 @@ sudo apt-get update
 sudo apt-get install dpkg-dev
 
 # install any packages needed for nginx
-sudo apt-get build-dep nginx-1.4.2
+sudo apt-get build-dep nginx=$NGINX_VERSION
 
 # deps for pagespeed
 sudo apt-get install build-essential zlib1g-dev libpcre3 libpcre3-dev libssl-dev
 
-# install the nginx sourc
+# psol lib (needed for pagespeed)
+wget https://dl.google.com/dl/page-speed/psol/1.6.29.5.tar.gz
+tar -xzvf 1.6.29.5.tar.gz # expands to psol/
+
+# install the nginx source
 mkdir build
 cd build
-apt-get source nginx-1.4.2
+apt-get source nginx=$NGINX_VERSION
+
+# copy our rules
+cp -f ../debian/rules nginx-${NGINX_VERSION}/debian/rules
+
+# build it
+cd nginx-${NGINX_VERSION} 
+dpkg-buildpackage -b
+
+echo ".deb built"
